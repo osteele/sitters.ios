ccc96335c4a858cea4b016b88be4dda3ebd33b42
Oliver Steele <steele@osteele.com>
2013-10-09 09:30:13 -0400

    move time selector methods to separate file

---
+++ app/controllers/time_selector.rb
@@ -0,0 +1,215 @@
+class SittersController < UIViewController
+  private
+  def createTimeSelector
+    weekStartDay = NSDate.date.dateAtStartOfDay
+
+    @timeSelectorView = subview TimeSelector, styleId: :time_selector do
+      dayLabelFormatter = dateFormatter('EEEE, MMMM d')
+      dayLabel = subview UILabel, styleClass: :date
+
+      dayHighlighter = subview UIButton, styleClass: :selected_day
+
+      overlays = []
+      weekdayDates = (0...7).map do |day| weekStartDay.dateByAddingDays(day) end
+      weekdayDates.each_with_index do |date, i|
+        x = 3 + i * 44
+        name = NSDateFormatter.alloc.init.setDateFormat('EEEEE').stringFromDate(date)
+        label = subview UILabel, text: name, styleClass: :day_of_week, left: x
+        overlay = subview UILabel, text: name, styleClass: 'day_of_week overlay', left: x
+        [label, overlay].each do |view|
+          view.when_tapped do
+            TestFlight.passCheckpoint "Tap day: #{name}"
+            # self.selectedTimeSpan = TimeSpan.new(date, selectedTimeSpan.startHour, selectedTimeSpan.endHour)
+            self.selectedTimeSpan = selectedTimeSpan.onDate(date)
+          end
+        end
+        overlays << overlay
+      end
+
+      observe(self, :selectedTimeSpan) do |previousTimeSpan, timeSpan|
+        # return if previousTimeSpan and previousTimeSpan.date == timeSpan.date
+        dayLabel.text = dayLabelFormatter.stringFromDate(timeSpan.date)
+        currentWeekDayIndex = weekdayDates.index(timeSpan.date)
+        selectedOverlay = overlays[currentWeekDayIndex]
+        UIView.animateWithDuration 0.3,
+          animations: lambda {
+            dayHighlighter.origin = [selectedOverlay.origin[0] + 5, selectedOverlay.origin[1]]
+            overlays.map do |v| v.alpha = 0 unless v == selectedOverlay end
+            selectedOverlay.alpha = 1
+          }
+      end
+
+      firstHourOffset = 10
+      firstHourNumber = 18
+      hourWidth = 58
+      [6, 7, 8, 9, 10, 11].each_with_index do |hour, i|
+        subview UIView, styleClass: :hour_blob, left: 10 + i * 58 do
+          # TODO use dateFormatter
+          subview UILabel, text: hour.to_s, styleClass: :hour
+          subview UILabel, text: 'PM', styleClass: :am_pm
+          subview UILabel, text: ':30', styleClass: :half_past
+        end
+      end
+
+      minHours = 1.5
+      range_label = nil
+      range_button = subview UIButton, styleClass: :hour_range, styleId: :hour_range do
+        range_label = subview UILabel, styleClass: :hour_range
+        range_label.autoresizingMask = UIViewAutoresizingFlexibleWidth
+
+        leftDragHandle = subview UIView, :left_dragger, styleClass: :left_dragger, styleId: :left_dragger do
+          subview UIView, styleClass: :graphic
+        end
+        rightDragHandle = subview UIView, :right_dragger, styleClass: :right_dragger, styleId: :right_dragger do
+          subview UIView, styleClass: :graphic
+        end
+
+        addDragger leftDragHandle, min: firstHourOffset, factor: hourWidth / 2
+        addResizer rightDragHandle, minWidth: minHours * hourWidth, factor: hourWidth / 2
+      end
+
+      # TODO use dateFormatter, to honor 24hr time. How to keep it from stripping the period?
+      hourMinuteFormatter = NSDateFormatter.alloc.init.setDateFormat('h:mm')
+      hourMinutePeriodFormatter = NSDateFormatter.alloc.init.setDateFormat('h:mma')
+      periodFormatter = NSDateFormatter.alloc.init.setDateFormat('a')
+      observe(self, :selectedTimeSpan) do |_, timeSpan|
+        startPeriod = periodFormatter.stringFromDate(timeSpan.startTime)
+        endPeriod = periodFormatter.stringFromDate(timeSpan.endTime)
+        startFormatter = if startPeriod == endPeriod then hourMinuteFormatter else hourMinutePeriodFormatter end
+        label = startFormatter.stringFromDate(timeSpan.startTime) + '-' + hourMinuteFormatter.stringFromDate(timeSpan.endTime) + ' ' + endPeriod
+        fontName = "HelveticaNeue"
+        string = NSMutableAttributedString.alloc.initWithString(label)
+        string.addAttribute NSFontAttributeName, value:UIFont.fontWithName(fontName + "-Bold", size:15), range:NSMakeRange(0, label.length)
+        string.addAttribute NSFontAttributeName, value:UIFont.fontWithName(fontName, size:8), range:NSMakeRange(label.length - 3, 1)
+        string.addAttribute NSFontAttributeName, value:UIFont.fontWithName(fontName, size:10), range:NSMakeRange(label.length - 2, 2)
+        range_label.attributedText = string
+      end
+
+      updater = Debounced.new 0.5 do
+        frame = range_button.frame
+        startHour = firstHourNumber + ((range_button.origin.x - firstHourOffset) / hourWidth * 2).round / 2.0
+        endHour = firstHourNumber + ((range_button.origin.x + range_button.size.width - firstHourOffset) / hourWidth * 2).round / 2.0 - 0.5
+        startHour = [startHour, firstHourNumber].max
+        endHour = [endHour, startHour + minHours].max
+        self.selectedTimeSpan = selectedTimeSpan.betweenTimes(startHour, endHour)
+      end
+
+      observe(range_button, :frame) do |_, frame|
+        updater.fire!
+      end
+    end
+  end
+
+  def addDragger(dragger, options={})
+    # class << dragger
+    #   def target; self.superview; end
+
+    #   def touchesBegan(touches, withEvent:event)
+    #     @initialOrigin = target.origin
+    #     @initialTouchPoint = touches.anyObject.locationInView(target.superview)
+    #   end
+
+    #   def touchesMoved(touches, withEvent:event)
+    #     touchPoint = touches.anyObject.locationInView(target.superview)
+    #     offset = CGPoint.new(touchPoint.x - @initialTouchPoint.x, touchPoint.y - @initialTouchPoint.y)
+    #     target.origin = [[0, @initialOrigin.x + offset.x].max, target.origin.y]
+    #   end
+    # end
+    # return
+
+    target = dragger.superview
+    initial = nil
+    dragger.userInteractionEnabled = true
+    dragger.when_panned do |recognizer|
+      pt = recognizer.translationInView(target.superview)
+      case recognizer.state
+      when UIGestureRecognizerStateBegan
+        initial = target.origin
+      when UIGestureRecognizerStateChanged
+        target.origin = [[initial.x + pt.x, options[:min] || 0].max, target.origin.y]
+      when UIGestureRecognizerStateEnded
+        min = options[:min] || 0
+        factor = options[:factor] || 1
+        x = ((target.origin.x - min) / factor).round * factor + min
+        UIView.animateWithDuration 0.1,
+          animations: lambda {
+            target.origin = [[x, options[:min] || 0].max, target.origin.y]
+          }
+      end
+    end
+  end
+
+  def addResizer(dragger, options={})
+    # class << dragger
+    #   def target; self.superview; end
+
+    #   def touchesBegan(touches, withEvent:event)
+    #     @initialSize = target.size
+    #     @initialTouchPoint = touches.anyObject.locationInView(target.superview)
+    #   end
+
+    #   def touchesMoved(touches, withEvent:event)
+    #     touchPoint = touches.anyObject.locationInView(target.superview)
+    #     offset = CGPoint.new(touchPoint.x - @initialTouchPoint.x, touchPoint.y - @initialTouchPoint.y)
+    #     target.size = [[@initialSize.width + offset.x, 100].max, target.size.height]
+    #     self.origin = [target.size.width - self.size.width, self.origin.y]
+    #   end
+    # end
+    # return
+
+    dragger.userInteractionEnabled = true
+    target = dragger.superview
+    initial = nil
+    fudge = 21
+    dragger.when_panned do |recognizer|
+      pt = recognizer.translationInView(target.superview)
+      case recognizer.state
+      when UIGestureRecognizerStateBegan
+        initial = target.size
+      when UIGestureRecognizerStateChanged
+        target.size = [[initial.width + pt.x, options[:minWidth] || 0].max, target.size.height]
+        dragger.origin = [target.size.width - dragger.size.width + fudge, dragger.origin.y]
+      when UIGestureRecognizerStateEnded
+        factor = options[:factor] || 1
+        width = (target.size.width / factor).round * factor
+        UIView.animateWithDuration 0.1,
+          animations: lambda {
+            target.size = [[width, options[:minWidth] || 0].max, target.size.height]
+            dragger.origin = [target.size.width - dragger.size.width + fudge, dragger.origin.y]
+          }
+      end
+    end
+  end
+end
+
+class TimeSpan
+  attr_reader :date, :startHour, :endHour
+
+  def initialize(date, startHour, endHour)
+    @date = date
+    @startHour = startHour
+    @endHour = endHour
+  end
+
+  def startTime
+    hourToTime(startHour)
+  end
+
+  def endTime
+    hourToTime(endHour)
+  end
+
+  def onDate(date)
+    TimeSpan.new(date, startHour, endHour)
+  end
+
+  def betweenTimes(startHour, endHour)
+    TimeSpan.new(date, startHour, endHour)
+  end
+
+  private
+
+  def hourToTime(hour)
+    date.dateByAddingHours(hour.floor).dateByAddingMinutes((hour * 60).floor % 60)
+  end
+end
