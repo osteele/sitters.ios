29a1ad4077492657e583dc1dbd40e3bff7547867
Oliver Steele <steele@osteele.com>
2013-10-10 08:10:28 -0400

    shrink the time selector when showing the suggested sitters

--- app/controllers/time_selector.rb
+++ app/controllers/time_selector.rb
@@ -2,6 +2,8 @@ class SittersController < UIViewController
   private
   def createTimeSelector
     weekStartDay = NSDate.date.dateAtStartOfDay
+    tallSizeOnlyViews = []
+    shortSizeOnlyViews = []
 
     @timeSelectorView = subview TimeSelector, styleId: :time_selector do
       dayLabelFormatter = dateFormatter('EEEE, MMMM d')
@@ -10,14 +12,17 @@ class SittersController < UIViewController
       firstDayX = 3
       dayspacing = 44
 
+      daySelectionMarker = nil
+      daySelectionMarkerOffset = 5
+      dayLabels = []
+      selectionMarkerLabels = []
+      weekdayDates = (0...7).map do |day| weekStartDay.dateByAddingDays(day) end
       daySelectionMarker = subview UIButton, styleClass: :selected_day do
         handle = subview UIView, width: 100, height: 100
         addDragger handle, min: firstDayX + 5, factor: dayspacing
       end
+      tallSizeOnlyViews << daySelectionMarker
 
-      dayLabels = []
-      selectionMarkerLabels = []
-      weekdayDates = (0...7).map do |day| weekStartDay.dateByAddingDays(day) end
       weekdayDates.each_with_index do |date, i|
         x = firstDayX + i * dayspacing
         name = NSDateFormatter.alloc.init.setDateFormat('EEEEE').stringFromDate(date)
@@ -34,8 +39,9 @@ class SittersController < UIViewController
         dayLabels << label
         selectionMarkerLabels << selectionMarkerLabel
       end
+      tallSizeOnlyViews += dayLabels
+      tallSizeOnlyViews += selectionMarkerLabels
 
-      daySelectionMarkerOffset = 5
       daySelectionMarker.superview.bringSubviewToFront daySelectionMarker
       selectionMarkerLabels.each do |label| label.superview.bringSubviewToFront label end
 
@@ -48,37 +54,41 @@ class SittersController < UIViewController
           date = weekdayDates[dayIndex]
           self.selectedTimeSpan = selectedTimeSpan.onDate(date) unless selectedTimeSpan.date == date
         end
-
       end
 
       observe(self, :selectedTimeSpan) do |previousTimeSpan, timeSpan|
-        # return if previousTimeSpan and previousTimeSpan.date == timeSpan.date
-        dayLabel.text = dayLabelFormatter.stringFromDate(timeSpan.date)
-        currentWeekDayIndex = weekdayDates.index(timeSpan.date)
-        selectedMarkerLabel = selectionMarkerLabels[currentWeekDayIndex]
-        UIView.animateWithDuration 0.3,
-          animations: lambda {
-            daySelectionMarker.origin = [selectedMarkerLabel.origin[0] + daySelectionMarkerOffset, selectedMarkerLabel.origin[1]]
-          }
+        unless previousTimeSpan and previousTimeSpan.date == timeSpan.date
+          dayLabel.text = dayLabelFormatter.stringFromDate(timeSpan.date)
+          currentWeekDayIndex = weekdayDates.index(timeSpan.date)
+          selectedMarkerLabel = selectionMarkerLabels[currentWeekDayIndex]
+          UIView.animateWithDuration 0.3,
+            animations: lambda {
+              daySelectionMarker.origin = [selectedMarkerLabel.origin[0] + daySelectionMarkerOffset, selectedMarkerLabel.origin[1]]
+            }
+          end
       end
 
       firstHourOffset = 10
       firstHourNumber = 18
       hourWidth = 58
-      [6, 7, 8, 9, 10, 11].each_with_index do |hour, i|
-        subview UIView, styleClass: :hour_blob, left: 10 + i * 58 do
-          # TODO use dateFormatter
-          subview UILabel, text: hour.to_s, styleClass: :hour
-          subview UILabel, text: 'PM', styleClass: :am_pm
-          subview UILabel, text: ':30', styleClass: :half_past
+      hoursView = subview UIView do
+        [6, 7, 8, 9, 10, 11].each_with_index do |hour, i|
+          subview UIView, styleClass: :hour_blob, left: 10 + i * 58 do
+            # TODO use dateFormatter
+            subview UILabel, text: hour.to_s, styleClass: :hour
+            subview UILabel, text: 'PM', styleClass: :am_pm
+            subview UILabel, text: ':30', styleClass: :half_past
+          end
         end
       end
+      tallSizeOnlyViews << hoursView
 
       minHours = 1.5
-      range_label = nil
-      range_button = subview UIButton, styleClass: :hour_range, styleId: :hour_range do
-        range_label = subview UILabel, styleClass: :hour_range
-        range_label.autoresizingMask = UIViewAutoresizingFlexibleWidth
+      hourRangeLabel = nil
+      leftDragHandle = rightDragHandle = nil
+      hourRangeButton = subview UIButton, styleClass: :hour_range, styleId: :hour_range do
+        hourRangeLabel = subview UILabel, styleClass: :hour_range
+        hourRangeLabel.autoresizingMask = UIViewAutoresizingFlexibleWidth
 
         leftDragHandle = subview UIView, :left_dragger, styleClass: :left_dragger, styleId: :left_dragger do
           subview UIView, styleClass: :graphic
@@ -90,6 +100,7 @@ class SittersController < UIViewController
         addDragger leftDragHandle, min: firstHourOffset, factor: hourWidth / 2
         addResizer rightDragHandle, minWidth: minHours * hourWidth, factor: hourWidth / 2
       end
+      tallSizeOnlyViews << hourRangeButton
 
       # TODO use dateFormatter, to honor 24hr time. How to keep it from stripping the period?
       hourMinuteFormatter = NSDateFormatter.alloc.init.setDateFormat('h:mm')
@@ -105,20 +116,37 @@ class SittersController < UIViewController
         string.addAttribute NSFontAttributeName, value:UIFont.fontWithName(fontName + "-Bold", size:15), range:NSMakeRange(0, label.length)
         string.addAttribute NSFontAttributeName, value:UIFont.fontWithName(fontName, size:8), range:NSMakeRange(label.length - 3, 1)
         string.addAttribute NSFontAttributeName, value:UIFont.fontWithName(fontName, size:10), range:NSMakeRange(label.length - 2, 2)
-        range_label.attributedText = string
+        hourRangeLabel.attributedText = string
       end
 
-      updater = Debounced.new 0.5 do
-        frame = range_button.frame
-        startHour = firstHourNumber + ((range_button.origin.x - firstHourOffset) / hourWidth * 2).round / 2.0
-        endHour = firstHourNumber + ((range_button.origin.x + range_button.size.width - firstHourOffset) / hourWidth * 2).round / 2.0 - 0.5
+      timeSpanHoursUpdater = Debounced.new 0.5 do
+        frame = hourRangeButton.frame
+        startHour = firstHourNumber + ((hourRangeButton.origin.x - firstHourOffset) / hourWidth * 2).round / 2.0
+        endHour = firstHourNumber + ((hourRangeButton.origin.x + hourRangeButton.size.width - firstHourOffset) / hourWidth * 2).round / 2.0 - 0.5
         startHour = [startHour, firstHourNumber].max
         endHour = [endHour, startHour + minHours].max
         self.selectedTimeSpan = selectedTimeSpan.betweenTimes(startHour, endHour)
       end
 
-      observe(range_button, :frame) do |_, frame|
-        updater.fire!
+      observe(hourRangeButton, :frame) do timeSpanHoursUpdater.fire! end
+
+      @shrinkTimeSelector = Proc.new do
+        @timeSelectorSaved = {
+          frame: @timeSelectorView.frame,
+          hourRangeColor: hourRangeButton.backgroundColor,
+          hourRangeFrame: hourRangeButton.frame,
+        }
+        @timeSelectorView.origin = [0, 50]
+        @timeSelectorView.size = [@timeSelectorView.size.width, 50]
+        tallSizeOnlyViews.each do |view| view.alpha = 0 end
+        shortSizeOnlyViews.each do |view| view.alpha = 1 end
+      end
+
+      @unshrinkTimeSelector = Proc.new do
+        @timeSelectorView.frame = @timeSelectorSaved[:frame]
+        hourRangeButton.frame = @timeSelectorSaved[:hourRangeFrame]
+        tallSizeOnlyViews.each do |view| view.alpha = 1 end
+        shortSizeOnlyViews.each do |view| view.alpha = 0 end
       end
     end
   end
