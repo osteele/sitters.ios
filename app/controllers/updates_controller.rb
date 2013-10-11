50196ded9191020d43bcf99af24cc850c4261ddb
Oliver Steele <steele@osteele.com>
2013-10-10 20:51:55 -0400

    improvements to event list

--- app/controllers/updates_controller.rb
+++ app/controllers/updates_controller.rb
@@ -6,27 +6,40 @@ class UpdatesController < UITableViewController
     end
   end
 
-  # layout do
-  #   search_bar = subview UISearchBar, frame: [0, 20, 320, 44], delegate: self
-  #   view.tableHeaderView = search_bar
-  # end
-
-  def numberOfSectionsInTableView(tableView)
-    1
-  end
+  def tableView(tableView, heightForHeaderInSection:section); 52; end
+  def numberOfSectionsInTableView(tableView); 1; end
 
   def tableView(tableView, numberOfRowsInSection:section)
     @sitters ||= Update.all
     @sitters.length
   end
 
+  IMAGE_TAG = 1
+  TITLE_TAG = 2
+  DESCRIPTION_TAG = 3
+  TIMESTAMP_TAG = 4
+
   def tableView(tableView, cellForRowAtIndexPath:indexPath)
     cellIdentifier = self.class.name
-    cell = tableView.dequeueReusableCellWithIdentifier(cellIdentifier) ||
-      UITableViewCell.alloc.initWithStyle(UITableViewCellStyleSubtitle, reuseIdentifier:cellIdentifier)
+    cell = tableView.dequeueReusableCellWithIdentifier(cellIdentifier)
+    fontName = "HelveticaNeue"
+    unless cell
+      cell = UITableViewCell.alloc.initWithStyle(UITableViewCellStyleSubtitle, reuseIdentifier:cellIdentifier)
+      layout cell.contentView do
+        subview UIImageView, tag: IMAGE_TAG, width: 47, height: 47, left: 10, top: 5
+        subview UILabel, tag: TITLE_TAG, width: 255, height: 40, left: 65, top: -3, textColor: 0x5988C4.uicolor
+        subview UILabel, tag: DESCRIPTION_TAG, width: 255, height: 40, left: 65, top: 14, font: UIFont.fontWithName(fontName, size:14)
+        subview UILabel, tag: TIMESTAMP_TAG, width: 200, height: 40, left: 110, top: -3,
+          font: UIFont.fontWithName(fontName, size:14), textAlignment: NSTextAlignmentRight
+      end
+    end
+
     update = Update.all[indexPath.row]
-    cell.textLabel.text = update.contact
-    cell.detailTextLabel.text = update.description
+
+    # cell.viewWithTag(IMAGE_TAG).image = update.maskedImage
+    cell.viewWithTag(TITLE_TAG).text = update.contact
+    cell.viewWithTag(DESCRIPTION_TAG).text = update.description
+    cell.viewWithTag(TIMESTAMP_TAG).text = update.timestamp
     cell
   end
 end
--- app/models/update.rb
+++ app/models/update.rb
@@ -5,15 +5,15 @@ class Update
 
   def self.all
     [
-      new("Steve and Diane Smith", "added a sitter", "4:12 PM"),
-      new("Maggie McConnell", "added a parent", "3:31 PM"),
-      new("Jane and Ted Phillips", "introduced you to Cindy King", "yesterday"),
-      new("Layla Smith", "added a parent", "yesterday"),
-      new("Gina Tarelli", "would like to be interviewed", "yesterday ago"),
-      new("Alex and Jen Handy", "added a sitter", "2d ago"),
-      new("Michelle Shaffer", "would like to be interviewed", "3d ago"),
-      new("Tim and Elisa Rendo", "added a sitter", "3d ago"),
-      new("Maggie McConnell", "added a parent", "5d ago"),
+      new('Steve and Diane Smith', 'added a sitter', '4:12 PM'),
+      new('Maggie McConnell', 'added a parent', '3:31 PM'),
+      new('Jane and Ted Phillips', 'introduced you to Cindy King', '2:48 PM'),
+      new('Layla Smith', 'added a parent', 'yesterday'),
+      new('Gina Tarelli', 'would like to be interviewed', 'yesterday'),
+      new('Alex and Jen Handy', 'added a sitter', '2d ago'),
+      new('Michelle Shaffer', 'would like to be interviewed', '3d ago'),
+      new('Tim and Elisa Rendo', 'added a sitter', '3d ago'),
+      new('Maggie McConnell', 'added a parent', '5d ago'),
     ]
   end
 
