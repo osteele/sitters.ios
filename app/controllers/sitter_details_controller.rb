cf466e7e86e4e72fd014af193ade3f5e8c3d15c8
Oliver Steele <steele@osteele.com>
2013-10-13 09:18:20 -0400

    sitter details scroll and size

--- app/controllers/sitter_details_controller.rb
+++ app/controllers/sitter_details_controller.rb
@@ -1,40 +1,40 @@
 class SitterDetailsController < UIViewController
   attr_accessor :sitter
-  attr_accessor :headerHeight
+  attr_accessor :webView
 
   layout do
-    self.headerHeight = 55
-
     view.styleId = :sitter_details
     view.backgroundColor = 0xF9F9F9.uicolor
 
+    headerHeight = 55
     view.top = headerHeight
     view.height -= headerHeight * 2
 
     url = NSBundle.mainBundle.URLForResource('sitter_details', withExtension:'html')
     @webView = subview UIWebView, delegate: self, frame: view.frame
-    renderTemplate sitter if sitter
+
+    renderTemplate if sitter
 
     # subview UILabel, styleId: :footer, text: 'Add to My Seven Sitters'
   end
 
   def sitter=(sitter)
+    return if @sitter == sitter
     @sitter = sitter
-    @webView.alpha = 0 if @webView # so we don't first see the previous sitter
-    renderTemplate sitter if @webView
+    renderTemplate if webView
   end
 
-  def renderTemplate(sitter)
-    @templatePath ||= NSBundle.mainBundle.pathForResource('sitter_details', ofType:'html')
-    @template ||= GRMustacheTemplate.templateFromContentsOfFile(@templatePath, error:nil)
+  def renderTemplate
+    webView.alpha = 0 if webView
+    @template ||= begin
+      templatePath ||= NSBundle.mainBundle.pathForResource('sitter_details', ofType:'html')
+      GRMustacheTemplate.templateFromContentsOfFile(templatePath, error:nil)
+    end
     html = @template.renderObject(sitter, error:nil)
-    @webView.loadHTMLString html, baseURL:NSURL.fileURLWithPath(NSBundle.mainBundle.bundlePath)
+    webView.loadHTMLString html, baseURL:NSURL.fileURLWithPath(NSBundle.mainBundle.bundlePath)
   end
 
   def webViewDidFinishLoad(webView)
-    view.frame = webView.frame
-    view.top = headerHeight
-
     webView.alpha = 1
   end
 end
