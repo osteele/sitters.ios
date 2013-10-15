725dbb40a7fd0848057bd9c49766b836e709bd6f
Oliver Steele <steele@osteele.com>
2013-10-15 08:17:01 -0400

    expired: more text; launch Safari

--- app/controllers/controllers.rb
+++ app/controllers/controllers.rb
@@ -9,6 +9,15 @@ end
 
 class ExpiredController < UIViewController
   layout do
-    subview UILabel, width: 320, height: 40, text: "Expired", textAlignment: NSTextAlignmentCenter, color: UIColor.whiteColor
+    textView = subview UITextView, width: 320, top: 20, height: 80, textAlignment: NSTextAlignmentCenter,
+      backgroundColor: UIColor.clearColor, textColor: UIColor.whiteColor,
+      text: "This application has expired. \nPlease tap below to install a newer version."
+    textView.font = textView.font.fontWithSize(18)
+
+    button = subview UIButton.buttonWithType(UIButtonTypeSystem), top: 240, width: 320, title: 'Update', textAlignment: NSTextAlignmentCenter
+    button.font = button.font.fontWithSize(28)
+    button.when_tapped do
+      UIApplication.sharedApplication.openURL NSURL.URLWithString('https://testflightapp.com/m/apps')
+    end
   end
 end
