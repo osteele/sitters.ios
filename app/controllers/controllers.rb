2b5657b18ac483b2d0482b8afb9e1aeec35ae3b4
Oliver Steele <steele@osteele.com>
2013-10-18 11:16:08 -0400

    map view in chat window (does not yet track location)

--- app/controllers/controllers.rb
+++ app/controllers/controllers.rb
@@ -1,9 +1,36 @@
 class ChatController < UIViewController
+  include BW::KVO
+  attr_reader :mapView
+
   def initWithNibName(name, bundle:bundle)
     super
     self.tap do
       self.tabBarItem = UITabBarItem.alloc.initWithTitle('Chat', image:UIImage.imageNamed('tabs/chat'), tag:4)
     end
+  end
+
+  def viewWillAppear(animated)
+    super
+    @locationManager ||= CLLocationManager.alloc.init.tap do |locationManager|
+      locationManager.delegate = self
+      locationManager.desiredAccuracy = KCLLocationAccuracyKilometer
+    end
+
+    @locationManager.startUpdatingLocation
+    Scheduler.after 10 do @locationManager.stopUpdatingLocation end
+  end
+
+  def locationManager(locationManager, didUpdateLocations:locations)
+    location = locations.last
+    locationManager.stopUpdatingLocation if 0 < location.horizontalAccuracy and location.horizontalAccuracy < 100
+    mapView.setRegion [location.coordinate, [1, 1]], animated:true
+    # mapView.centerCoordinate = location.coordinate
+  end
+
+  layout do
+    @mapView = subview MKMapView.alloc.initWithFrame([[0, 0],[320, 122]]),
+      showsUserLocation: true,
+      userTrackingMode: MKUserTrackingModeFollow
   end
 end
 
