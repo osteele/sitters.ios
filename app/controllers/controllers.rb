1478652e3ebde1799c0013d050a5f2f6cf71cd3d
Oliver Steele <steele@osteele.com>
2013-10-18 10:18:14 -0400

    fix the expiration screen

--- app/controllers/controllers.rb
+++ app/controllers/controllers.rb
@@ -9,13 +9,17 @@ end
 
 class ExpiredController < UIViewController
   layout do
-    textView = subview UITextView, width: 320, top: 20, height: 80, textAlignment: NSTextAlignmentCenter,
-      backgroundColor: UIColor.clearColor, textColor: UIColor.whiteColor,
-      text: "This application has expired. \nPlease tap below to install a newer version."
+    textView = subview UITextView, top: 20, width: 320, height: 80,
+      editable: false,
+      textAlignment: NSTextAlignmentCenter,
+      backgroundColor: UIColor.clearColor,
+      textColor: UIColor.whiteColor,
+      text: "This application has expired."
     textView.font = textView.font.fontWithSize(18)
 
-    button = subview UIButton.buttonWithType(UIButtonTypeSystem), top: 240, width: 320, title: 'Update', textAlignment: NSTextAlignmentCenter
+    button = subview UIButton.buttonWithType(UIButtonTypeSystem), top: 240, width: 320, height: 50, title: 'Tap to update'
     button.font = button.font.fontWithSize(28)
+
     button.when_tapped do
       UIApplication.sharedApplication.openURL NSURL.URLWithString('https://testflightapp.com/m/apps')
     end
