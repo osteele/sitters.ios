6c3ec75085ede65abf3f78af9be58991157663c7
Oliver Steele <steele@osteele.com>
2013-10-29 11:09:22 -0400

    change the build date format (in settings)

--- config/settings.rb
+++ config/settings.rb
@@ -1,4 +1,4 @@
 Motion::SettingsBundle.setup do |app|
   app.title 'Build version', key: :build_date, default: BUILD_VERSION
-  app.title 'Build date', key: :build_date, default: BUILD_DATE.strftime('%H:%M %b %d, %Y')
+  app.title 'Build date', key: :build_date, default: BUILD_DATE.strftime('%I:%M %p %x')
 end
