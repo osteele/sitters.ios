1d8001c24939a6aded7bfedbb6701973d158baaf
Oliver Steele <steele@osteele.com>
2013-11-06 19:29:17 -0500

    rake was setting TESTFLIGHT_APP_TOKEN, but app was reading TF_APP_TOKEN

--- app/app_delegate.rb
+++ app/app_delegate.rb
@@ -105,7 +105,7 @@ class AppDelegate
   def initializeTestFlight
     return if Device.simulator?
     # return unless Object.const_defined?(:TestFlight)
-    app_token = NSBundle.mainBundle.objectForInfoDictionaryKey('TF_APP_TOKEN')
+    app_token = NSBundle.mainBundle.objectForInfoDictionaryKey('TESTFLIGHT_APP_TOKEN')
     # TODO remove call to TestFlight.setDeviceIdentifier before submitting to app store
     TestFlight.setDeviceIdentifier UIDevice.currentDevice.uniqueIdentifier
     TestFlight.takeOff app_token
