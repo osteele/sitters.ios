c5669e8ab80589af87bb5366f561ffac213b69b2
Oliver Steele <steele@osteele.com>
2013-11-27 15:20:53 -0500

    add XCode project

---
+++ Seven Sitters/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Seven Sitters
+//
+//  Created by Oliver Steele on 11/23/13.
+//  Copyright (c) 2013 Oliver Steele. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
