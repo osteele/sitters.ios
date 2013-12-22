7f8dfaba0c9a8102e19a32920a27611ca988d46a
Oliver Steele <steele@osteele.com>
2013-12-22 17:31:09 -0500

    restore 'rake spec'

--- app/app_delegate.rb
+++ app/app_delegate.rb
@@ -12,7 +12,7 @@ class AppDelegate
   attr_accessor :window
 
   def application(application, didFinishLaunchingWithOptions:launchOptions)
-    return true if RUBYMOTION_ENV == 'test'
+    # return true if RUBYMOTION_ENV == 'test'
     # TODO: process launchOptions[UIApplicationLaunchOptionsLocalNotificationKey] ?
 
     DDLog.addLogger DDASLLogger.sharedInstance
